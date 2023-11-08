dd6516be75369147a14bb7830187e4d58c40d3a1
Ezra Keshet <ezrakeshet@gmail.com>
2023-11-08 13:01:20 -0500

    Update __init__.py

--- ling123/__init__.py
+++ ling123/__init__.py
@@ -9,3 +9,26 @@ def pp(x):
     print(textwrap.fill(x))
   else:
     pprint.pp(x, compact=True)
+
+from contextlib import contextmanager
+@contextmanager
+def mock_input(mocked_responses):
+    # Keep a reference to the original input function
+    original_input = __builtins__.input
+    
+    # Define a generator to yield mock responses
+    response_gen = (response for response in mocked_responses)
+    
+    # This function will replace 'input'
+    def input_mock(prompt=""):
+        # Get the next response from the generator
+        return next(response_gen)
+    
+    # Replace the built-in input with our mock
+    __builtins__.input = input_mock
+    
+    try:
+        yield  # Control to the block inside 'with' statement
+    finally:
+        # Restore the original input function
+        __builtins__.input = original_input
