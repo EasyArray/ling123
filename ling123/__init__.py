3f5759c84edf0a02250242e9f1cf8f2644cbefaf
Ezra Keshet <ezrakeshet@gmail.com>
2023-11-08 12:25:26 -0500

    Update __init__.py to add pp

--- ling123/__init__.py
+++ ling123/__init__.py
@@ -1,3 +1,13 @@
 from .magics import print123
 from .formal import all_theorems, theorem_tree, get_outputs
 from .grader import Grader
+
+import pprint
+import textwrap
+def pp(x):
+  if type(x) == str:
+    print(textwrap.fill(x))
+  else:
+    pprint.pp(x, compact=True)
+
+pp(mad_lib)
